0839d688c15ce004c9c0e0423e6a6b987351d8aa
sahanagana <sahaana.ganapathy@gmail.com>
2023-07-11 23:06:47 -0500

    load window

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-//main file
+
+//imports from electron
+const { BrowserWindow, app } = require("electron")
+
+//function that creates a window when the application is opened
+const createWindow = () => {
+
+	//window characteristics
+	const mainWindow = new BrowserWindow({
+		width: 900,
+		height: 700
+	})
+	
+	//html file to load for window
+	mainWindow.loadFile("index.html");
+};
+
+//app module controls lifecycle of app
+app.whenReady().then(createWindow); //once electron is initialized
