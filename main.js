52fa06fc55897606b76ba51b849d73ba13f898c9
sahanagana <sahaana.ganapathy@gmail.com>
2023-07-11 23:11:19 -0500

    add development tools

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 //imports from electron
 const { BrowserWindow, app } = require("electron")
 
+//electron reloader setup
+require("electron-reloader")(module);
+
 //function that creates a window when the application is opened
 const createWindow = () => {
 
@@ -11,6 +14,8 @@ const createWindow = () => {
 		height: 700
 	})
 	
+	//automatically open devtools
+	mainWindow.webContents.openDevTools();
 	//html file to load for window
 	mainWindow.loadFile("index.html");
 };
